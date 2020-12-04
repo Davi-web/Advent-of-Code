c5c5ecf371a11033e9d1c5e77205159632bcd1c1
haj1 <jeseung.ha@vanderbilt.edu>
2020-12-04 02:43:56 -0800

    Adding file

--- AdventOfCode/day2.py
+++ AdventOfCode/day2.py
@@ -2,11 +2,10 @@ with open("day2-input.txt") as f:
     lines = f.readlines()
     word = []
     for line in lines:
-
         word += line.split()
 
 if __name__ == '__main__':
-    print(word)
+
     j = 1
     k = 2
 
@@ -25,15 +24,4 @@ if __name__ == '__main__':
             res += 1
         j += 3
         k += 3
-    print(res)
-    # for i in range(0, len(word), 3):
-    #     low, high = word[i].split('-')
-    #     char, temp2 = word[j].split(':')
-    #
-    #     password = word[k]
-    #     print(low, high, char, password, password.count(char))
-    #     if int(low) <= password.count(char) <= int(high):
-    #         count += 1
-    #     j += 3
-    #     k += 3
-    # print(count)
+    print("Ans:", res)
